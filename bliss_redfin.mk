cbedda27e210202c5ffcf965dc8e6518ceba2b5c
Jackeagle <jackeagle102@gmail.com>
2024-05-14 23:17:14 -0400

    Blissify redfin
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_redfin.mk
+++ bliss_redfin.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/redfin/aosp_redfin.mk)
@@ -16,7 +16,7 @@ include device/google/redfin/device-lineage.mk
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 5
-PRODUCT_NAME := lineage_redfin
+PRODUCT_NAME := bliss_redfin
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
